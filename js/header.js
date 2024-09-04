7529dd21e4a02e1c8bfefae4a74ee17bb5a4fc54
ooootazzir <83293723+rizzatoVinicius@users.noreply.github.com>
2024-09-03 21:58:58 -0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -3,7 +3,7 @@ var list = document.querySelector(".header__list")
 function menu(){
         list.classList.toggle("list__active")
         document.body.style.overflow = "hidden"
-
+        abriMenu.style.display = "none"
 }
 
 var abriMenu = document.getElementById("header__button")
